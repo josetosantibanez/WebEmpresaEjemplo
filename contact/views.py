0e2e380d19e9226ee2897a999d01e93adcb89110
jose_santibanez <jtsantiba@gmail.com>
2020-05-23 18:05:13 -0400

    Segundo commit

--- contact/views.py
+++ contact/views.py
@@ -5,30 +5,33 @@ from .forms import ContactForms
 
 # Create your views here.
 
+
 def contact(request):
     contact_form = ContactForms()
 
-                                                
     if request.method == "POST":
         contact_form = ContactForms(data=request.POST)
         if contact_form.is_valid():
             name = request.POST.get('name', '')
             email = request.POST.get('email', '')
             content = request.POST.get('content', '')
-            #Todo fue bien, enviamos correo y redireccionamos
+            # Todo fue bien, enviamos correo y redireccionamos
             email = EmailMessage(
-                "La caffetiera: Nuevo mensaje de contacto",#asunto
-                "De {} <{}>\n\nEscribio:\n\n{}".format(name, email, content),#cuerpo
-                "no-contestar@inbox.mailtrap.io",#email_origen
-                ["xerigazon1997@gmail.com"],#email_destino
-                reply_to=[email]#reply_to=[email]
-                )
-            try:                                            
+                "La caffetiera: Nuevo mensaje de contacto",  # asunto
+                "De {} <{}>\n\nEscribio:\n\n{}".format(
+                    name, email, content),  # cuerpo
+                "no-contestar@inbox.mailtrap.io",  # email_origen
+                ["xerigazon1997@gmail.com"],  # email_destino
+                reply_to=[email]  # reply_to=[email]
+            )
+            try:
                 email.send()
-                #Todo ha ido bien, redireccionamos a OK
+                # Todo ha ido bien, redireccionamos a OK
                 return redirect(reverse('contact')+"?ok")
             except:
-                #Todo ha ido bien, redireccionamos a FAIL
+                # Todo ha ido bien, redireccionamos a FAIL
                 return redirect(reverse('contact')+"?fail")
 
-    return render(request,'contact/contact.html',{'form':contact_form})
+    # Aca vamos a poner un comentario para ver que cambia la cosa
+
+    return render(request, 'contact/contact.html', {'form': contact_form})
